27d414e05c587f83115752ff83ab62b2e65d9de9
JosenildoMauricio <mauricius.contact@gmail.com>
2024-09-25 02:03:46 -0300

    correções e implementado chamada a api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import Sistema from "@/components/Sistema";
-import { list } from "@/data/list";
+import { Diretorio } from "@/types/Diretorio";
+// import { list } from "@/data/dados";
+
+import { useEffect, useState } from "react";
 
 function Page() {
+
+    const [carregando, setCarregando] = useState(true);
+    const [diretorio, setDiretorio] = useState<Diretorio>();
+
+    const getDados = async () => {
+        setCarregando(true);
+        try {
+            const res = await fetch("http://localhost:8080/api/diretorios/raiz");
+            const json = await res.json();
+            setDiretorio(json);
+        } catch (error) {
+            console.log(`Erro ao buscar diretórios. ${error}`)
+        } finally {
+            setCarregando(false);
+        }
+    }
+
+    useEffect(() => {
+        getDados();
+    },[])
 
     const appStyle: React.CSSProperties = {
         display: 'flex',
@@ -17,11 +42,19 @@ function Page() {
     return(
         <div style={appStyle}>
             <h1 className="App" style={titleStyle}>Explorador de Arquivos</h1>
-            {list && (
+            {carregando &&
+                <h1 style={{ marginLeft: '40px' }}>Carregando…</h1>
+            }
+            {!carregando && diretorio && (
                 <>
-                    <Sistema diretorio={list}/>
+                    {diretorio &&(
+                        <Sistema diretorio={diretorio}/>
+                    )}
                 </>
             )}
+            {!carregando && diretorio === undefined && 
+                <h1 style={{ marginLeft: '40px' }}>Erro ao carregar dados.</h1>
+            }
         </div>
     );
 }
---
+++ src/data/dados.ts
@@ -0,0 +1,150 @@
+import { Diretorio } from "@/types/Diretorio";
+
+export const list: Diretorio = 
+{
+    "id": 1,
+    "nome": "DIRETORIO RAIZ",
+    "diretorioPai": null,
+    "subDiretorios": [
+        {
+            "id": 2,
+            "nome": "Documentos",
+            "subDiretorios": [
+                {
+                    "id": 5,
+                    "nome": "Contas a pagar",
+                    "subDiretorios": [],
+                    "arquivos": [
+                        {
+                            "id": 6,
+                            "nome": "contas - Janeiro.docx",
+                            "dataCadastro": "2024-09-25T03:09:24Z",
+                            "dataAtualizacao": "2024-09-25T03:09:24Z"
+                        }
+                    ],
+                    "dataCadastro": "2024-09-25T03:07:24Z",
+                    "dataAtualizacao": "2024-09-25T03:07:24Z"
+                },
+                {
+                    "id": 6,
+                    "nome": "Vários documentos",
+                    "subDiretorios": [
+                        {
+                            "id": 8,
+                            "nome": "Fotos viagens",
+                            "subDiretorios": [],
+                            "arquivos": [
+                                {
+                                    "id": 9,
+                                    "nome": "001.jpg",
+                                    "dataCadastro": "2024-09-25T03:12:59Z",
+                                    "dataAtualizacao": "2024-09-25T03:12:59Z"
+                                },
+                                {
+                                    "id": 10,
+                                    "nome": "002.jpg",
+                                    "dataCadastro": "2024-09-25T03:13:05Z",
+                                    "dataAtualizacao": "2024-09-25T03:13:05Z"
+                                },
+                                {
+                                    "id": 11,
+                                    "nome": "08-03-2024.png",
+                                    "dataCadastro": "2024-09-25T03:13:30Z",
+                                    "dataAtualizacao": "2024-09-25T03:13:30Z"
+                                }
+                            ],
+                            "dataCadastro": "2024-09-25T03:12:31Z",
+                            "dataAtualizacao": "2024-09-25T03:12:31Z"
+                        }
+                    ],
+                    "arquivos": [
+                        {
+                            "id": 7,
+                            "nome": "atestado medico.pdf",
+                            "dataCadastro": "2024-09-25T03:10:39Z",
+                            "dataAtualizacao": "2024-09-25T03:10:39Z"
+                        },
+                        {
+                            "id": 8,
+                            "nome": "lista de endereços úteis.txt",
+                            "dataCadastro": "2024-09-25T03:11:07Z",
+                            "dataAtualizacao": "2024-09-25T03:11:07Z"
+                        }
+                    ],
+                    "dataCadastro": "2024-09-25T03:07:50Z",
+                    "dataAtualizacao": "2024-09-25T03:07:50Z"
+                },
+                {
+                    "id": 7,
+                    "nome": "Certidões",
+                    "subDiretorios": [],
+                    "arquivos": [
+                        {
+                            "id": 12,
+                            "nome": "certidão de titulo eleitoral.pdf",
+                            "dataCadastro": "2024-09-25T03:14:45Z",
+                            "dataAtualizacao": "2024-09-25T03:14:45Z"
+                        }
+                    ],
+                    "dataCadastro": "2024-09-25T03:07:59Z",
+                    "dataAtualizacao": "2024-09-25T03:07:59Z"
+                }
+            ],
+            "arquivos": [
+                {
+                    "id": 5,
+                    "nome": "lista de compras.txt",
+                    "dataCadastro": "2024-09-25T03:06:27Z",
+                    "dataAtualizacao": "2024-09-25T03:06:27Z"
+                }
+            ],
+            "dataCadastro": "2024-09-25T03:02:44Z",
+            "dataAtualizacao": "2024-09-25T03:02:44Z"
+        },
+        {
+            "id": 3,
+            "nome": "Musicas",
+            "subDiretorios": [],
+            "arquivos": [
+                {
+                    "id": 1,
+                    "nome": "minha musica.mp3",
+                    "dataCadastro": "2024-09-25T03:04:49Z",
+                    "dataAtualizacao": "2024-09-25T03:04:49Z"
+                },
+                {
+                    "id": 2,
+                    "nome": "musica.mp3",
+                    "dataCadastro": "2024-09-25T03:04:57Z",
+                    "dataAtualizacao": "2024-09-25T03:04:57Z"
+                }
+            ],
+            "dataCadastro": "2024-09-25T03:02:53Z",
+            "dataAtualizacao": "2024-09-25T03:02:53Z"
+        },
+        {
+            "id": 4,
+            "nome": "Vídeos",
+            "subDiretorios": [],
+            "arquivos": [
+                {
+                    "id": 3,
+                    "nome": "um filme.mp4",
+                    "dataCadastro": "2024-09-25T03:05:29Z",
+                    "dataAtualizacao": "2024-09-25T03:05:29Z"
+                },
+                {
+                    "id": 4,
+                    "nome": "filme caseiro.mp4",
+                    "dataCadastro": "2024-09-25T03:05:44Z",
+                    "dataAtualizacao": "2024-09-25T03:05:44Z"
+                }
+            ],
+            "dataCadastro": "2024-09-25T03:03:17Z",
+            "dataAtualizacao": "2024-09-25T03:03:17Z"
+        }
+    ],
+    "arquivos": [],
+    "dataCadastro": "2024-09-25T02:47:58Z",
+    "dataAtualizacao": "2024-09-25T02:47:58Z"
+};
